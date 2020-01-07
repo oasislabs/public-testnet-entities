74d763434a656f8551437e86b77f3e6566d4fcc8
Reuven V. Gonzales <reuven@oasislabs.com>
2020-01-06 16:28:49 -0800

    Fixes for genesis testing scripts

--- .github/scripts/python/generate_genesis.py
+++ .github/scripts/python/generate_genesis.py
@@ -7,19 +7,26 @@ def main():
     # Find all of the entity_genesis.json files and node_genesis.json files
     unpacked_entities_path = os.path.abspath(sys.argv[1])
 
+    # Hacky overrides for running locally.
+    output_path = os.environ.get("GENESIS_OUTPUT_PATH", "/tmp/genesis.json")
+    staking_path = os.environ.get("STAKING_GENESIS_PATH", "/tmp/staking.json")
+    oasis_node_path = os.environ.get("OASIS_NODE_PATH", '/tmp/oasis-node')
+
     genesis_command = [
-        "oasis-node",
-        "--genesis.file", "/tmp/genesis.json",
+        oasis_node_path, "genesis", "init",
+        "--genesis.file", output_path,
         "--chain.id", "sometest-chain-id",
-        "--staking", "/tmp/staking.json",
+        "--staking", staking_path,
         "--epochtime.tendermint.interval", "200",
-        "--epochtime.tendermint.timeout_commit", "5s",
+        "--consensus.tendermint.timeout_commit", "5s",
         "--consensus.tendermint.empty_block_interval", "0s",
         "--consensus.tendermint.max_tx_size", "32kb",
-        "--consensus.tendermint.backend", "tendermint"
+        "--consensus.backend", "tendermint"
     ]
 
     for entity_name in os.listdir(unpacked_entities_path):
+        if os.path.isfile(os.path.join(unpacked_entities_path, entity_name)):
+            continue
         genesis_command.extend([
             "--entity", os.path.join(unpacked_entities_path,
                                      entity_name, "entity/entity_genesis.json"),
